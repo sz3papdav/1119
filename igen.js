3f66e6fe0f41b951e3470d15dea8c5a48974af3d
sz3papdav <144676937+sz3papdav@users.noreply.github.com>
2023-11-19 14:37:01 +0100

    Add files via upload

---
+++ igen.js
@@ -0,0 +1,32 @@
+let car = ['Ford', 'Mustang', '2022', 'blue'];
+let [brand, model,year,color ] = car;
+console.log(brand);
+console.log(model);
+console.log(year);
+console.log(color);
+
+let book =
+{
+    title: 'Javascript: The Good Parts',
+    author : 'Douglas Crackford',
+    publicationYear: 2008,
+    language: 'English'
+};
+
+let{title: bookTitle,author:bookAuthor,publicationYear,language} = book;
+console.log(bookTitle);
+console.log(bookAuthor);
+console.log(publicationYear);
+console.log(language);
+
+function printStudentInfo({  name, age, grade, subjects}) {
+    console.log(`${name}, ${grade}, ${subjects}, ${age} éves`);
+  }
+  
+  let student = {
+    name: 'John Doe',
+    age: 20,
+    grade: 'B',
+    subjects: ['Math', 'English', 'History']
+  };
+  printStudentInfo(student);
